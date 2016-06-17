567557026bb437761bf368c8b0fd532622567f52
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-06-17 14:34:33 -0700

    Initial Commit

---
+++ AlcatrazIt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AlcatrazIt
+//
+//  Created by Francesco Perrotti-Garcia on 6/17/16.
+//  Copyright © 2016 Francesco Perrotti-Garcia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AlcatrazIt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AlcatrazIt
+//
+//  Created by Francesco Perrotti-Garcia on 6/17/16.
+//  Copyright © 2016 Francesco Perrotti-Garcia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
