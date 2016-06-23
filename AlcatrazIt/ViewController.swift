47097355c40fc119f64f5e2755ca7209c5bced0d
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-06-23 20:57:03 -0300

    Add working DragView

--- AlcatrazIt/ViewController.swift
+++ AlcatrazIt/ViewController.swift
@@ -5,23 +5,20 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let xcode = Xcode(url: URL(fileURLWithPath: "/Users/fpg1503/Downloads/Xcode/Xcode-beta.app/"))
-        
+        if let dragView = view as? DragView {
+            dragView.delegate = self
+        }
+    }
+}
+
+extension ViewController: DragViewDelegate {
+    var acceptedFileExtensions: [String] { return ["app"] }
+    func dragView(_ dragView: DragView, didDragFileWith fileURL: URL) {
+        let xcode = Xcode(url: fileURL)
         if let xcodeGreat = xcode.makeGreatAgain() {
-            print("WOHOOL! \(xcodeGreat)")
+            print("WOO HOO! \(xcodeGreat)")
         } else {
             print("Not this time, brah")
         }
-        
-        
-        // Do any additional setup after loading the view.
     }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
 }
-
