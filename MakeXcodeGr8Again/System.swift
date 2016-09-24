4cf3bc71ae4b29b5d8dd348a489b26400d6e5edb
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-09-24 07:48:57 -0300

    Add System

---
+++ MakeXcodeGr8Again/System.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+private extension Pipe {
+    func stringsToEndOfFile(encoding: String.Encoding = .ascii) -> [String] {
+        let data = fileHandleForReading.readDataToEndOfFile()
+        let contents = String(data: data, encoding: encoding) ?? ""
+
+        let clearContents = contents.trimmingCharacters(in: .newlines)
+
+        return clearContents.components(separatedBy: "\n")
+    }
+}
+
+struct System {
+
+    static func run(command: String, args : String...) -> (output: [String], error: [String], exitCode: Int32) {
+
+        let task = Process()
+        task.launchPath = command
+        task.arguments = args
+
+        let outputPipe = Pipe()
+        task.standardOutput = outputPipe
+        let errorPipe = Pipe()
+        task.standardError = errorPipe
+
+        task.launch()
+
+        let output = outputPipe.stringsToEndOfFile()
+        let error = errorPipe.stringsToEndOfFile()
+        
+        task.waitUntilExit()
+        let status = task.terminationStatus
+        
+        return (output, error, status)
+    }
+}
