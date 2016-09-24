995517e5818078ac8eeeeeec8d1f203285d0d002
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-09-24 08:21:35 -0300

    Reindent unsign

--- MakeXcodeGr8Again/Unsign/unsign.m
+++ MakeXcodeGr8Again/Unsign/unsign.m
@@ -1,26 +1,26 @@
 /*
-  unsign: remove code signing from Mach-O and Universal Binary files.
+ unsign: remove code signing from Mach-O and Universal Binary files.
 
-  This program removes the LC_CODE_SIGNATURE load command and
-  zeroes out the signature in the __LINKEDIT section.
+ This program removes the LC_CODE_SIGNATURE load command and
+ zeroes out the signature in the __LINKEDIT section.
 
-  TODO: handle EOF errors better
-  TODO: handle assumption failures with more grace
+ TODO: handle EOF errors better
+ TODO: handle assumption failures with more grace
 
-  Copyright (c) 2010
+ Copyright (c) 2010
 
-  Permission to use, copy, modify, and/or distribute this software for any
-  purpose with or without fee is hereby granted, provided that the above
-  copyright notice and this permission notice appear in all copies.
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
 
-  The software is provided "as is" and the author disclaims all warranties
-  with regard to this software including all implied warranties of
-  merchantability and fitness. In no event shall the author be liable for
-  any special, direct, indirect, or consequential damages or any damages
-  whatsoever resulting from loss of use, data or profits, whether in an
-  action of contract, negligence or other tortious action, arising out of
-  or in connection with the use or performance of this software.
-*/
+ The software is provided "as is" and the author disclaims all warranties
+ with regard to this software including all implied warranties of
+ merchantability and fitness. In no event shall the author be liable for
+ any special, direct, indirect, or consequential damages or any damages
+ whatsoever resulting from loss of use, data or profits, whether in an
+ action of contract, negligence or other tortious action, arising out of
+ or in connection with the use or performance of this software.
+ */
 
 #include "endian.h"
 
@@ -39,224 +39,218 @@
 #include <mach-o/loader.h>
 
 #ifdef DEBUG
-    #define DEBUG_UNSIGN
+#define DEBUG_UNSIGN
 #endif
 
 #ifdef DEBUG_UNSIGN
-    #define PRINT_DEBUG printf
+#define PRINT_DEBUG printf
 #else
-    #define PRINT_DEBUG //
+#define PRINT_DEBUG //
 #endif
 
 #define BUF_SZ 4096
 
-static void
-expect(bool b, const char* s) {
-        if (!b) {
-                perror(s);
-                abort();
+static void expect(bool b, const char* s) {
+    if (!b) {
+        perror(s);
+        abort();
+    }
+}
+
+static size_t fzero(size_t size, size_t count, FILE *stream) {
+    for (size_t i = 0; i < count; i++) {
+        for (size_t j = 0; j < size; j++) {
+            if (fputc(0, stream) == EOF) {
+                return i;
+            }
         }
+    }
+    return count;
 }
 
-static size_t
-fzero(size_t size, size_t count, FILE *stream) {
-        for (size_t i = 0; i < count; i++) {
-                for (size_t j = 0; j < size; j++) {
-                        if (fputc(0, stream) == EOF) {
-                                return i;
-                        }
-                }
+static void fcopy(size_t size, FILE *in, FILE *out, const char *infile, const char *outfile) {
+    uint8_t buffer[512];
+    while (size) {
+        size_t to_copy = size;
+        if(to_copy > sizeof(buffer)) {
+            to_copy = sizeof(buffer);
         }
-        return count;
+        expect(fread(&buffer, 1, to_copy, in) == to_copy, infile);
+        expect(fwrite(&buffer, 1, to_copy, out) == to_copy, outfile);
+        size -= to_copy;
+    }
 }
 
-static void
-fcopy(size_t size, FILE *in, FILE *out, const char *infile, const char *outfile) {
-        uint8_t buffer[512];
-        while (size) {
-                size_t to_copy = size;
-                if(to_copy > sizeof(buffer)) {
-                        to_copy = sizeof(buffer);
-                }
-                expect(fread(&buffer, 1, to_copy, in) == to_copy, infile);
-                expect(fwrite(&buffer, 1, to_copy, out) == to_copy, outfile);
-                size -= to_copy;
-        }
+static inline uintmax_t to_align(uintmax_t n, uintmax_t a) {
+    uintmax_t mask = a - 1;
+
+    assert((a & mask) == 0);
+    return (a - (n & mask)) & mask;
 }
 
-static inline uintmax_t
-to_align(uintmax_t n, uintmax_t a) {
-        uintmax_t mask = a - 1;
+static void align_file(FILE* f, const char *name, uintmax_t align) {
+    off_t o = ftello(f);
+    expect(o != -1, name);
 
-        assert((a & mask) == 0);
-        return (a - (n & mask)) & mask;
+    size_t to_write = to_align(o, align);
+    expect(fzero(1, to_write, f) == to_write, name);
 }
 
-static void
-align_file(FILE* f, const char *name, uintmax_t align) {
-        off_t o = ftello(f);
-        expect(o != -1, name);
+static void macho_unsign(FILE *in, FILE *out, const char *infile, const char *outfile, off_t size) {
+    off_t start = ftello(in);
+    expect(start != -1, infile);
 
-        size_t to_write = to_align(o, align);
-        expect(fzero(1, to_write, f) == to_write, name);
-}
+    uint8_t magicb[4];
+    expect(fread(&magicb, sizeof(magicb), 1, in) == 1, infile);
+    uint32_t magicbe = be32dec(&magicb);
+    uint32_t magicle = le32dec(&magicb);
 
-static void
-macho_unsign(FILE *in, FILE *out, const char *infile, const char *outfile, off_t size) {
-        off_t start = ftello(in);
-        expect(start != -1, infile);
+    bool big_endian, sixtyfourbits;
+    if (magicbe == MH_MAGIC) {
+        big_endian = true;
+        sixtyfourbits = false;
+    } else if (magicbe == MH_MAGIC_64) {
+        big_endian = true;
+        sixtyfourbits = true;
+    } else if (magicle == MH_MAGIC) {
+        big_endian = false;
+        sixtyfourbits = false;
+    } else if (magicle == MH_MAGIC_64) {
+        big_endian = false;
+        sixtyfourbits = true;
+    } else {
+        fprintf(stderr, "Unknown mach-o magic number %02x %02x %02x %02x\n",
+                magicb[0], magicb[1], magicb[2], magicb[3]);
+        abort();
+    }
 
-        uint8_t magicb[4];
-        expect(fread(&magicb, sizeof(magicb), 1, in) == 1, infile);
-        uint32_t magicbe = be32dec(&magicb);
-        uint32_t magicle = le32dec(&magicb);
+    expect(fseeko(in, start, SEEK_SET) != -1, infile);
 
-        bool big_endian, sixtyfourbits;
-        if (magicbe == MH_MAGIC) {
-                big_endian = true;
-                sixtyfourbits = false;
-        } else if (magicbe == MH_MAGIC_64) {
-                big_endian = true;
-                sixtyfourbits = true;
-        } else if (magicle == MH_MAGIC) {
-                big_endian = false;
-                sixtyfourbits = false;
-        } else if (magicle == MH_MAGIC_64) {
-                big_endian = false;
-                sixtyfourbits = true;
+    uint32_t (*x32dec)(const void*) = big_endian ? be32dec : le32dec;
+    void (*x32enc)(void*, uint32_t) = big_endian ? be32enc : le32enc;
+
+    uint32_t ncmds;
+    uint32_t sizeofcmds;
+    if (sixtyfourbits) {
+        struct mach_header_64 header;
+        expect(fread(&header, sizeof(header), 1, in) == 1, infile);
+        ncmds = x32dec(&header.ncmds);
+        sizeofcmds = x32dec(&header.sizeofcmds);
+        x32enc(&header.ncmds, ncmds - 1);
+        x32enc(&header.sizeofcmds, sizeofcmds - sizeof(struct linkedit_data_command));
+        expect(fwrite(&header, sizeof(header), 1, out) == 1, outfile);
+    } else {
+        struct mach_header header;
+        expect(fread(&header, sizeof(header), 1, in) == 1, infile);
+        ncmds = x32dec(&header.ncmds);
+        sizeofcmds = x32dec(&header.sizeofcmds);
+        x32enc(&header.ncmds, ncmds - 1);
+        x32enc(&header.sizeofcmds, sizeofcmds - sizeof(struct linkedit_data_command));
+        expect(fwrite(&header, sizeof(header), 1, out) == 1, outfile);
+    }
+
+    uint32_t dataoff = 0, datasize = 0;
+    for (uint32_t i = 0; i < ncmds; i++) {
+        off_t lc_start = ftello(in);
+        expect(lc_start != -1, infile);
+
+        struct load_command lc;
+        expect(fread(&lc, sizeof(lc), 1, in) == 1, infile);
+        uint32_t cmd = x32dec(&lc.cmd);
+        uint32_t cmdsize = x32dec(&lc.cmdsize);
+
+        expect(fseeko(in, lc_start, SEEK_SET) != -1, infile);
+        if (cmd != LC_CODE_SIGNATURE) {
+            fcopy(cmdsize, in, out, infile, outfile);
         } else {
-                fprintf(stderr, "Unknown mach-o magic number %02x %02x %02x %02x\n",
-                        magicb[0], magicb[1], magicb[2], magicb[3]);
-                abort();
+            printf("    found LC_CODE_SIGNATURE\n");
+            assert(dataoff == 0);
+            struct linkedit_data_command lc_sig;
+            assert(cmdsize == sizeof(lc_sig));
+            expect(fread(&lc_sig, sizeof(lc_sig), 1, in) == 1, infile);
+            dataoff = x32dec(&lc_sig.dataoff);
+            assert(dataoff != 0);
+            datasize = x32dec(&lc_sig.datasize);
         }
-
-        expect(fseeko(in, start, SEEK_SET) != -1, infile);
-
-        uint32_t (*x32dec)(const void*) = big_endian ? be32dec : le32dec;
-        void (*x32enc)(void*, uint32_t) = big_endian ? be32enc : le32enc;
-
-        uint32_t ncmds;
-        uint32_t sizeofcmds;
-        if (sixtyfourbits) {
-                struct mach_header_64 header;
-                expect(fread(&header, sizeof(header), 1, in) == 1, infile);
-                ncmds = x32dec(&header.ncmds);
-                sizeofcmds = x32dec(&header.sizeofcmds);
-                x32enc(&header.ncmds, ncmds - 1);
-                x32enc(&header.sizeofcmds, sizeofcmds - sizeof(struct linkedit_data_command));
-                expect(fwrite(&header, sizeof(header), 1, out) == 1, outfile);
-        } else {
-                struct mach_header header;
-                expect(fread(&header, sizeof(header), 1, in) == 1, infile);
-                ncmds = x32dec(&header.ncmds);
-                sizeofcmds = x32dec(&header.sizeofcmds);
-                x32enc(&header.ncmds, ncmds - 1);
-                x32enc(&header.sizeofcmds, sizeofcmds - sizeof(struct linkedit_data_command));
-                expect(fwrite(&header, sizeof(header), 1, out) == 1, outfile);
-        }
-
-        uint32_t dataoff = 0, datasize = 0;
-        for (uint32_t i = 0; i < ncmds; i++) {
-                off_t lc_start = ftello(in);
-                expect(lc_start != -1, infile);
-
-                struct load_command lc;
-                expect(fread(&lc, sizeof(lc), 1, in) == 1, infile);
-                uint32_t cmd = x32dec(&lc.cmd);
-                uint32_t cmdsize = x32dec(&lc.cmdsize);
-
-                expect(fseeko(in, lc_start, SEEK_SET) != -1, infile);
-                if (cmd != LC_CODE_SIGNATURE) {
-                        fcopy(cmdsize, in, out, infile, outfile);
-                } else {
-                        printf("    found LC_CODE_SIGNATURE\n");
-                        assert(dataoff == 0);
-                        struct linkedit_data_command lc_sig;
-                        assert(cmdsize == sizeof(lc_sig));
-                        expect(fread(&lc_sig, sizeof(lc_sig), 1, in) == 1, infile);
-                        dataoff = x32dec(&lc_sig.dataoff);
-                        assert(dataoff != 0);
-                        datasize = x32dec(&lc_sig.datasize);
-                }
-        }
-        assert(dataoff != 0);
-        expect(fzero(sizeof(struct linkedit_data_command), 1, out) == 1, outfile);
-        off_t after_lc = ftello(in);
-        expect(after_lc != -1, infile);
-        fcopy(dataoff - (after_lc - start), in, out, infile, outfile);
-        expect(fzero(1, datasize, out) == datasize, outfile);
-        expect(fseeko(in, datasize, SEEK_CUR) != -1, infile);
-        off_t after_data = ftello(in);
-        expect(after_data != -1, infile);
-        fcopy(size - (after_data - start), in, out, infile, outfile);
+    }
+    assert(dataoff != 0);
+    expect(fzero(sizeof(struct linkedit_data_command), 1, out) == 1, outfile);
+    off_t after_lc = ftello(in);
+    expect(after_lc != -1, infile);
+    fcopy(dataoff - (after_lc - start), in, out, infile, outfile);
+    expect(fzero(1, datasize, out) == datasize, outfile);
+    expect(fseeko(in, datasize, SEEK_CUR) != -1, infile);
+    off_t after_data = ftello(in);
+    expect(after_data != -1, infile);
+    fcopy(size - (after_data - start), in, out, infile, outfile);
 }
 
 static void
 ub_unsign(FILE *in, FILE *out, const char *infile, const char *outfile, off_t size) {
-        uint8_t magicb[4];
-        expect(fread(&magicb, sizeof(magicb), 1, in) == 1, infile);
-        if (be32dec(&magicb) != FAT_MAGIC) {
-                expect(! fseeko(in, 0, SEEK_SET), infile);
-                macho_unsign(in, out, infile, outfile, size);
-                printf("not a fat binary\n");
-                return;
-        }
+    uint8_t magicb[4];
+    expect(fread(&magicb, sizeof(magicb), 1, in) == 1, infile);
+    if (be32dec(&magicb) != FAT_MAGIC) {
+        expect(! fseeko(in, 0, SEEK_SET), infile);
+        macho_unsign(in, out, infile, outfile, size);
+        printf("not a fat binary\n");
+        return;
+    }
 
-        uint8_t nfat_archb[4];
-        expect(fread(&nfat_archb, sizeof(nfat_archb), 1, in) == 1, infile);
+    uint8_t nfat_archb[4];
+    expect(fread(&nfat_archb, sizeof(nfat_archb), 1, in) == 1, infile);
 
-        expect(fwrite(&magicb, sizeof(magicb), 1, out) == 1, outfile);
-        expect(fwrite(&nfat_archb, sizeof(nfat_archb), 1, out) == 1, outfile);
+    expect(fwrite(&magicb, sizeof(magicb), 1, out) == 1, outfile);
+    expect(fwrite(&nfat_archb, sizeof(nfat_archb), 1, out) == 1, outfile);
 
-        uint32_t nfat_arch = be32dec(&nfat_archb);
+    uint32_t nfat_arch = be32dec(&nfat_archb);
 
-        off_t outarcho = ftello(out);
+    off_t outarcho = ftello(out);
+    expect(outarcho != -1, outfile);
+
+    expect(fzero(sizeof(struct fat_arch), nfat_arch, out) == nfat_arch, outfile);
+
+    for (uint32_t i = 0; i < nfat_arch; i++) {
+        printf("  processing fat architecture %d of %d\n", i+1, nfat_arch);
+        struct fat_arch arch;
+        expect(fread(&arch, sizeof(arch), 1, in) == 1, infile);
+        off_t inarcho = ftello(in);
+        expect(inarcho != -1, infile);
+
+        uint32_t alignment = be32dec(&arch.align);
+        assert(alignment < 32);
+        alignment = 1 << alignment;
+        align_file(out, outfile, alignment);
+
+        expect(! fseeko(in, be32dec(&arch.offset), SEEK_SET), infile);
+
+        off_t offset = ftello(out);
+
+        macho_unsign(in, out, infile, outfile, be32dec(&arch.size));
+
+        expect(! fseeko(in, inarcho, SEEK_SET), infile);
+
+        off_t end = ftello(out);
+        expect(end != -1, outfile);
+        off_t size = end - offset;
+
+        errno = ERANGE;
+        uint32_t uoffset = (uint32_t)offset;
+        expect(uoffset == offset, "offset");
+        uint32_t usize = (uint32_t)size;
+        expect(usize == size, "size");
+
+        be32enc(&arch.offset, uoffset);
+        be32enc(&arch.size, usize);
+
+        off_t outo = ftello(out);
+        expect(outo != -1, outfile);
+
+        expect(! fseeko(out, outarcho, SEEK_SET), outfile);
+        expect(fwrite(&arch, sizeof(arch), 1, out) == 1, outfile);
+        outarcho = ftello(out);
         expect(outarcho != -1, outfile);
-
-        expect(fzero(sizeof(struct fat_arch), nfat_arch, out) == nfat_arch, outfile);
-
-        for (uint32_t i = 0; i < nfat_arch; i++) {
-                printf("  processing fat architecture %d of %d\n", i+1, nfat_arch);
-                struct fat_arch arch;
-                expect(fread(&arch, sizeof(arch), 1, in) == 1, infile);
-                off_t inarcho = ftello(in);
-                expect(inarcho != -1, infile);
-
-                uint32_t alignment = be32dec(&arch.align);
-                assert(alignment < 32);
-                alignment = 1 << alignment;
-                align_file(out, outfile, alignment);
-
-                expect(! fseeko(in, be32dec(&arch.offset), SEEK_SET), infile);
-
-                off_t offset = ftello(out);
-
-                macho_unsign(in, out, infile, outfile, be32dec(&arch.size));
-
-                expect(! fseeko(in, inarcho, SEEK_SET), infile);
-
-                off_t end = ftello(out);
-                expect(end != -1, outfile);
-                off_t size = end - offset;
-
-                errno = ERANGE;
-                uint32_t uoffset = (uint32_t)offset;
-                expect(uoffset == offset, "offset");
-                uint32_t usize = (uint32_t)size;
-                expect(usize == size, "size");
-
-                be32enc(&arch.offset, uoffset);
-                be32enc(&arch.size, usize);
-
-                off_t outo = ftello(out);
-                expect(outo != -1, outfile);
-
-                expect(! fseeko(out, outarcho, SEEK_SET), outfile);
-                expect(fwrite(&arch, sizeof(arch), 1, out) == 1, outfile);
-                outarcho = ftello(out);
-                expect(outarcho != -1, outfile);
-                expect(! fseeko(out, outo, SEEK_SET), outfile);
-        }
+        expect(! fseeko(out, outo, SEEK_SET), outfile);
+    }
 }
 
 boolean_t is_unsigned(char *infile) {
@@ -267,7 +261,7 @@ boolean_t is_unsigned(char *infile) {
 void unsign(char *infile, char *outfile) {
 
     PRINT_DEBUG("%s - %s", infile, outfile);
-    
+
     expect(outfile, "allocate");
 
     int infd = open(infile, O_RDONLY);
@@ -298,12 +292,12 @@ void unsign(char *infile, char *outfile) {
 
     fseek(outtmp, 0, SEEK_SET);
     do {
-      size_t bytes_read = fread(buf, 1, BUF_SZ, outtmp);
+        size_t bytes_read = fread(buf, 1, BUF_SZ, outtmp);
 
-      if (bytes_read > 0) {
-        size_t bytes_written = fwrite(buf, 1, bytes_read, outactual);
-        expect(bytes_written == bytes_read, "didnt write output file completely");
-      }
+        if (bytes_read > 0) {
+            size_t bytes_written = fwrite(buf, 1, bytes_read, outactual);
+            expect(bytes_written == bytes_read, "didnt write output file completely");
+        }
     } while (!feof(outtmp));
 
     PRINT_DEBUG("wrote outfile: %s\n", outfile);
